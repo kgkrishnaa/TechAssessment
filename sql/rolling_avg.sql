3f58871ca5f94193d5b561729214c4be88116f82
kgkrishnaa <kgkrishnaa@gmail.com>
2024-09-25 17:14:22 +0100

    Added screenshots

--- sql/rolling_avg.sql
+++ sql/rolling_avg.sql
@@ -1,10 +1,11 @@
-
-
 -- Rolling average sql 
+-- This is not a true rolling average and needs more work
+
 SELECT * , round ( avg(OrderValue)
 OVER (
-        ORDER BY orderdate
+        PARTITION BY customerid
+        ORDER BY orderdate        
         ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
     ) , 1)  AS rolling_avg_30_days 
     FROM `plasma-ripple-261523.DataSet_Orders.Orders`
-order by CustomerId, OrderID
+order by CustomerId, orderdate
